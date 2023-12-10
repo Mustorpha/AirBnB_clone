3c3ae9f1d4a01a4ef724e971f3bde1c25678c040
mustorpha <mustorphajamiu@gmail.com>
2023-12-10 03:26:10 -0800

    the storage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for FileStorage class."""
+
 import datetime
 import json
 import os
@@ -8,26 +9,31 @@ import os
 class FileStorage:
 
     """Class for storing and retrieving data"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """returns the dictionary __objects"""
+
         return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
+
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path)"""
+
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
 
     def classes(self):
         """Returns a dictionary of valid classes and their references"""
+
         from models.base_model import BaseModel
         from models.user import User
         from models.state import State
@@ -53,11 +59,11 @@ class FileStorage:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
 
     def attributes(self):
         """Returns the valid attributes and their types for classname"""
+
         attributes = {
             "BaseModel":
                      {"id": str,
