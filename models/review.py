6e7d27c48e2f3b8f0ceb1d8726be2cf3e0b968bd
MrVeeJon <victor.jonathan.u@gmail.com>
2023-12-10 08:30:00 -0800

    models/place.py

--- models/review.py
+++ models/review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Extends the base model for reviews"""
+"""Extends base model for reviews"""
 
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Class for managing review objects"""
+    """Class to manage review objects"""
 
     place_id = ""
     user_id = ""
